6f10ba505e25acad197a61bec1d3c8b9851e5a37
avlasova05 <avlasva@mail.ru>
2025-07-04 11:34:42 +0300

    Update init.py

--- mmf-archive/app/init.py
+++ mmf-archive/app/init.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 from config import Config
-
+import os
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static', 'images')
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     app.mysql=MySQL(app)
 
     from app.routes import students, subjects, grades, attendance, debts
@@ -17,6 +18,7 @@ def create_app():
 
     return app
 app = create_app()
-
+if __name__ == '__main__':
+    app.run(debug=True)
 if __name__ == '__main__':
     app.run(debug=True)
