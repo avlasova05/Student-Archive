c2b9ca33914c5703abf8ef33e2ffd40513732e67
avlasova05 <avlasva@mail.ru>
2025-07-07 12:55:59 +0300

    Update init.py

--- mmf-archive/app/init.py
+++ mmf-archive/app/init.py
@@ -5,11 +5,13 @@ import os
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.secret_key = os.environ.get('FLASK_SECRET_KEY') or os.urandom(24)
     app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static', 'images')
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     app.mysql=MySQL(app)
 
-    from app.routes import students, subjects, grades, attendance, debts
+    from app.routes import students, subjects, grades, attendance, debts, main
+    app.register_blueprint(main.bp)
     app.register_blueprint(students.bp)
     app.register_blueprint(subjects.bp)
     app.register_blueprint(grades.bp)
